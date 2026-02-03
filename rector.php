e1bbff5878175c9b9c6766e3146241cdf1d490d0
thePanz <thepanz@gmail.com>
2026-02-03 16:31:32 +0100

    chore(tools): Upgrade Rector configuration

--- rector.php
+++ rector.php
@@ -2,41 +2,31 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
+use Rector\CodingStyle\Rector\ClassLike\NewlineBetweenClassLikeStmtsRector;
+use Rector\CodingStyle\Rector\ClassMethod\NewlineBeforeNewAssignSetRector;
+use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\TypeDeclaration\Rector\Class_\PropertyTypeFromStrictSetterGetterRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\BoolReturnTypeFromStrictScalarReturnsRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\NumericReturnTypeFromStrictScalarReturnsRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNativeCallRector;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
+    ])
+    ->withCache('.cache/rector')
+    ->withImportNames(importShortClasses: false)
+    ->withParallel()
+    ->withPhpSets()
 
-    $rectorConfig->rules([
-        BoolReturnTypeFromStrictScalarReturnsRector::class,
-        InlineConstructorDefaultToPropertyRector::class,
-        PropertyTypeFromStrictSetterGetterRector::class,
-        TypedPropertyFromStrictConstructorRector::class,
-        NumericReturnTypeFromStrictScalarReturnsRector::class,
-        ReturnTypeFromStrictNativeCallRector::class
-    ]);
+    ->withSkip([
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+        CatchExceptionNameMatchingTypeRector::class,
+        NewlineAfterStatementRector::class,
+        NewlineBeforeNewAssignSetRector::class,
+        NewlineBetweenClassLikeStmtsRector::class,
+    ])
 
-    $rectorConfig->sets([
-        SetList::PHP_81,
-        PHPUnitSetList::PHPUNIT_100,
-        # \Rector\Set\ValueObject\SetList::CODE_QUALITY
-    ]);
-
-    // use imports instead of FQN: https://github.com/rectorphp/rector/blob/main/docs/auto_import_names.md#auto-import-names
-    $rectorConfig->importNames();
-    $rectorConfig->cacheDirectory('.cache/rector');
-
-    // Do not import Single short classes:
-    $rectorConfig->importShortClasses(false);
-};
+    ->withCodeQualityLevel(20)
+    ->withPreparedSets(deadCode: true, codingStyle: true, earlyReturn: true)
+;
